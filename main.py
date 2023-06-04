ea0321d95574b0f1e200f715f34ce06fe0a13a4a
rootkitthis <git@mikemazade.com>
2023-06-04 18:14:29 -0500

    Small clean up of spaces

--- main.py
+++ main.py
@@ -26,24 +26,27 @@ image2 = src + "/*.JPG"
 image3 = src + "/*.jpeg"
 image4 = src + "/*.JPEG"
 dev0 = src + "/*.py"
-
 # Define functions to move screenshots, PNG images, and documents to their respective folders
 def mv_screenshot():
     for files in glob.iglob(ss):
         shutil.move(files, screenshot)
+
+
 def mv_images():
     for files in glob.iglob(image0):
         shutil.move(files, png)
     for files in glob.iglob(image00):
         shutil.move(files, png)
     for files in glob.iglob(image1):
-        shutil.move(files,png)
+        shutil.move(files, png)
     for files in glob.iglob(image2):
         shutil.move(files, png)
     for files in glob.iglob(image3):
         shutil.move(files, png)
     for files in glob.iglob(image4):
         shutil.move(files, png)
+
+
 def mv_docs():
     for files in glob.iglob(doc0):
         shutil.move(files, documents)
@@ -61,10 +64,13 @@ def mv_docs():
         shutil.move(files, documents)
     for files in glob.iglob(doc7):
         shutil.move(files, documents)
+
+
 def mv_dev():
     for files in glob.iglob(dev0):
         shutil.move(files, developer)
         print('Moved the following items(s):', files)
+
 
 # Call the functions to move the files
 print(mv_screenshot())
